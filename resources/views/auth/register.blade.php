f574889acda233d8e6c70948530feabf8c9d0c6f
Vittorio <v.fazio6410@gmail.com>
2024-07-10 12:28:21 +0200

    aggiunta authentication

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+    <x-navbar></x-navbar>
+
+    <div class="container">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="mt-3 mb-3 row justify-content-center">
+                <div class="col-12 text-center">
+                    <h1>Register</h1>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="name" name="name" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Password Confirmation</label>
+                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
+            </div>
+            <button type="submit" class="btn btn-dark">Register</button>
+        </form>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+</x-layout>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          @auth
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ciao {{ Auth::user()->name }}</a>
+          </li>
+          <li class="nav-item">
+            <form action="{{route('logout')}}" method="POST">
+              @csrf
+              <button class="nav-link">Logout</button>
+            </form>
+          </li>
+          @endauth
+          @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+          @endguest
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
