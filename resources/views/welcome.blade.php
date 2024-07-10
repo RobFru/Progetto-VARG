1cb2a5439fa23526f080e93c5cae1f77d9d47ff8
Morandre95 <andreamorelli95@outlook.com>
2024-07-10 17:31:41 +0200

    User story 1 completata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
 
     <x-masthead/>
+<div>
+    <a href="{{route('create.article')}}"><button type="submit" class="button-86" role="button">Create Articles</button></a>
+</div>
 
 </x-layout>
