b025c7d646670cd4f418881cec7bc3634d73ffd1
Morandre95 <andreamorelli95@outlook.com>
2024-07-10 17:53:22 +0200

    User story 1 completata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
 <x-layout>
 
     <x-masthead/>
-<div>
-    <a href="{{route('create.article')}}"><button type="submit" class="button-86" role="button">Create Articles</button></a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            <a href="{{route('create.article')}}"><button type="submit" class="button-86" role="button">Create Articles</button></a>
+        </div>
+    </div>
 </div>
 
 </x-layout>
