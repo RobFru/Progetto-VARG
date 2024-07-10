00ed1a691dbc375ec7423b5b41d4d27795292a2e
Roberta Frulli <rob.frulli@gmail.com>
2024-07-10 13:00:28 +0200

    Aggiunta modello per categorie e categorie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
+    public $categories=[
+        'furniture',
+        'watches',
+        'shoes',
+        'clothing',
+        'paintings',
+        'books',
+        'jewelry',
+        'electronics',
+        'vehicles',
+        'music',
+
+    ];
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       
+        foreach ($this->categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
     }
 }
